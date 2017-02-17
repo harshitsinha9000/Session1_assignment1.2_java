e541683de3009526696db2c453076b6edc607ffa
harshitsinha9000 <harshitsinha9000@gmail.com>
2017-02-17 13:07:57 +0530

    Add files via upload

---
+++ Second.java
@@ -0,0 +1,18 @@
+package java_session1;
+
+public class Second {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int a=20,b=10;
+		int b1=(a--)-(--a);
+		int c=a--;
+		int d=a>>2;
+		int e=a&b1;
+		
+		System.out.println("a="+a+"\nb1="+b1+"\nc="+c+"\nd="+d+"\ne="+e);
+
+	}
+
+}
